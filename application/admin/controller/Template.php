836a57ab338928723608dae4512142dc882dea27
wangzheng <nsssh@163.com>
2020-01-10 15:03:25 +0800

    模板编辑改写，限制目录

--- application/admin/controller/Template.php
+++ application/admin/controller/Template.php
@@ -3,6 +3,7 @@ namespace app\admin\controller;
 
 use think\facade\Request;
 use think\facade\Lang;
+use think\facade\Env;
 use think\exception\HttpException;
 use app\common\model\Site;
 use app\admin\controller\Admin;
@@ -23,49 +24,39 @@ class Template extends Admin
 
     public function fileedit()
     {
-        if (Request::isAjax()) {
-            $request = Request::param();
-            $encode = mb_detect_encoding($request['html'], array('ASCII', 'UTF-8', 'GB2312', 'GBK', 'BIG5'));
-            $request['path'] = base64_decode($request['path']);
-            if ($encode != 'UTF-8') {
-                $request['html'] = iconv($encode, 'UTF-8', $request['html']);
-            }
+        $path = Request::param('path');
+        $siteObj = new Site;
+        $template = $siteObj->where('id', $this->site_id)->value('theme');
+        $rootpath = Env::get('root_path') . 'theme' . DIRECTORY_SEPARATOR . $template . DIRECTORY_SEPARATOR . $path;
+        // 判断文件是否存在
+        if (!file_exists($rootpath) && !preg_match("/theme/", $rootpath)) {
+            throw new HttpException(404, 'This is not file');
+        }
 
-            if (file_exists($request['path']) && preg_match("/theme/", $request['path'])) {
-                if (is_writable($request['path'])) {
-                    $html = file_put_contents($request['path'], htmlspecialchars_decode($request['html']));
-                } else {
-                    throw new HttpException(404, 'File not readabled');
-                }
+        if (Request::isPost()) {
+            if (is_writable($rootpath)) {
+                $html = file_put_contents($rootpath, htmlspecialchars_decode(Request::param('html')));
             } else {
-                throw new HttpException(404, 'This is not file');
+                throw new HttpException(404, 'File not readabled');
             }
-
             if ($html) {
                 return $this->response(200, Lang::get('Success'));
             } else {
                 return $this->response(201, Lang::get('Fail'));
             }
-        }
-
-        $request = Request::param('path');
-        $path = base64_decode($request);
-
-        if (file_exists($path) && preg_match("/theme/", $path)) {
-            if (is_readable($path)) {
-                $html = file_get_contents($path);
+        } else {
+            if (is_readable($rootpath)) {
+                $html = file_get_contents($rootpath);
             } else {
                 throw new HttpException(404, 'File not readabled');
             }
-        } else {
-            throw new HttpException(404, 'This is not file');
+            $data = [
+                'html' => htmlspecialchars($html),
+                'path' => $path,
+                'name' => base64_decode(Request::param('name')),
+            ];
+    
+            return $this->fetch('fileedit', $data);
         }
-        $data = [
-            'html' => htmlspecialchars($html),
-            'path' => $request,
-            'name' => base64_decode(Request::param('name')),
-        ];
-
-        return $this->fetch('fileedit', $data);
     }
 }
